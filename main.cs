0262e821742c8f3a38cb3ad6bc7acc0ed42346f2
Arun Kumar V <arkumarmailbox@gmail.com>
2020-10-01 10:59:48 +0000

    testing

--- main.cs
+++ main.cs
@@ -7,7 +7,7 @@ class MainClass {
     List<string> sobj = new List<string>()
      {"arun", "aaron", "kumar", "kawasaki", "satish", "basavaraj", "brisbane"};
    var query = sobj
-                .Where(s => s.Length >5)
+                .Where(s => s.Length >6)
                 .Select(s => s)
                 .Where((s, i) => { return s.Length >3; })
                 .OrderByDescending(s => s.Length)
