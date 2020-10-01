6c779b7a7d89eb9141352004d61050628dfc67bb
Arun Kumar <arkumarmailbox@gmail.com>
2020-10-01 10:38:27 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class MainClass {
+  public static void Main (string[] args) {
+    List<string> sobj = new List<string>()
+     {"arun", "aaron", "kumar", "kawasaki", "satish", "basavaraj", "brisbane"};
+   var query = sobj
+                //.Where(s => s.Length >4)
+                .Select(s => s)
+                .Where((s, i) => { return s.Length >3; })
+                .OrderByDescending(s => s.Length)
+                .ThenByDescending(s => s);
+   foreach(string name in query){
+     Console.WriteLine(name);
+   }
+  }
+}
